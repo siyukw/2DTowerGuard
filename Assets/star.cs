c20f4da125894e03d79c5d5a7bb32f903038ca84
Siyu Kat Wang <siyukw@gmail.com>
2017-04-23 14:22:43 -0700

    changed panel's layer

--- Assets/star.cs
+++ Assets/star.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class star : MonoBehaviour {
-	public static int speed;
-	public Vector3 userDirection;
+
 
 	// Use this for initialization
 	void Start () {
-		speed = 1;
-		userDirection = new Vector3(0.5, -1);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position;
+		
 	}
 }
 
