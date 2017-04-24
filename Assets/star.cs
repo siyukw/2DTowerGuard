9890b5568a8e8d3b6765d81c69092ef92afbf014
Siyu Kat Wang <siyukw@gmail.com>
2017-04-23 20:07:21 -0700

    changed light to snowflakes; adjusted size; added soot and sunlight

--- Assets/star.cs
+++ Assets/star.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class star : MonoBehaviour {
-
+	private static int speed;
 
 	// Use this for initialization
 	void Start () {
-
+		speed = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Translate (Vector3.down * speed * Time.deltaTime);
 	}
 }
 
