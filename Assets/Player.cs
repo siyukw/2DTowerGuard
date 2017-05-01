c2f9045cbde13d5b863098404207e8fb5c750b69
Siyu Kat Wang <siyukw@gmail.com>
2017-05-01 16:28:19 -0700

    did calculation for updat

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	private float speed;
+	//public Text timer;
+
+
 	// Use this for initialization
 	void Start () {
 		speed = 3;
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		
 		float axisX = Input.GetAxis("Horizontal");
 		float axisY = Input.GetAxis("Vertical");
 		transform.Translate(new Vector3(axisX, axisY) * Time.deltaTime * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Destroy (other.gameObject);
 	}
 }
